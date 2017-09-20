02d85aad11cdc614275da7c0d8480ec0044a2e58
Buker, Nick <Nick.Buker@nordstrom.com>
2017-09-20 11:21:16 -0700

    update to use sapply

--- src/monte_hall.R
+++ src/monte_hall.R
@@ -1,23 +1,23 @@
 monte_hall <- function(switch_door, n_sim=10000) {
     doors <- c(0, 0, 1)
-    results <- c()
-    return(mean(play_game(switch_door, doors, results, n_sim)))
-}
+    results <- rep(0, n_sim)
 
-play_game <- function(switch_door, doors, results, n_sim) {
-    for (sim in seq(n_sim)) {
+    play_game <- function(n) {
         doors <- sample(doors)
         if (switch_door) {
             if (doors[2] == 0) {
-                results <- c(results, doors[3])
+                n <- doors[3]
             }
             else {
-                results <- c(results, doors[2])
+                n <- doors[2]
             }
         }
         else {
-            results <- c(results, doors[1])
+            n <- doors[1]
         }
+        return (n)
     }
-    return(results)
+
+    results <- sapply(results, play_game)
+    return(mean(results))
 }
